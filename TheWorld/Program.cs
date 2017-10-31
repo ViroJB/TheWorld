5750f2eae2d726674e63a03b296ae34e8060216e
Julian <julian@mobile-ad-media.com>
2017-10-31 20:57:07 +0100

    first commit

---
+++ TheWorld/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Viro
+ * Date: 30.10.2017
+ * Time: 20:54
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TheWorld.entity;
+
+namespace TheWorld
+{
+	class Program
+	{
+		
+		const int ticks = 4;
+		const int delay = 500;
+		
+		public static void Main(string[] args)
+		{
+			// TODO arraylist von animals die arraylisten der einzelnen tiere enthält
+			
+			Random r = new Random();
+			var animals = new ArrayList();
+			var dogs = new List<Dog>();
+			
+			animals.Add(dogs);
+			
+			int i = 0;
+			while(true) {
+				animals.Add(new Dog());
+				animals.Item
+				dog.setId(i);
+				dog.setAge(r.Next(0,5));
+				dog.setMaxAge(r.Next(12,30));
+				if(r.Next(0,2) == 1) {
+					dogs[i].setSex("m");
+				} else {
+					dogs[i].setSex("w");
+				}
+				
+				if(i % ticks == 0) {
+					
+				}
+				
+				Console.Write(dogs[i].getId() + ". " + dogs[i].getSex() + " " + dogs[i].getAge() + " " + dogs[i].getMaxAge() + "\n");
+				++i;
+				Thread.Sleep(delay);
+			}
+		}
+	}
+}
---
+++ TheWorld/entity/animal.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Viro
+ * Date: 30.10.2017
+ * Time: 20:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TheWorld.entity
+{
+	/// <summary>
+	/// Description of animal.
+	/// </summary>
+	public class Animal
+	{
+		
+		private int id;
+		private int age = 0;
+		private int maxAge = 0;
+		
+		public Animal()
+		{
+		}
+		
+		public void setId(int id) {
+			this.id = id;
+		}
+		
+		public int getId() {
+			return this.id;
+		}
+		
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+		public int getAge() {
+			return this.age;
+		}
+		
+		public void setMaxAge(int maxAge) {
+			this.maxAge = maxAge;
+		}
+		
+		public int getMaxAge() {
+			return this.maxAge;
+		}
+	}
+}
